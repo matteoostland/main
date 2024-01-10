3152871938647f7a76722779eb956db499a964d9
tkelley3 <th.kelley@northeastern.edu>
2024-01-10 16:32:24 -0500

    Jan 10 Update

---
+++ indentation.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 10 15:31:35 2024
+
+@author: tomke
+"""
+
+def main():
+    numbers_list=[1,1,2,3,5,8]
+    total=0
+    for num in numbers_list:
+        total += num
+    print("Current total:",total)
+    return total
+ 
+    
+main()
