16853e95625b48551c03cf779c3aa57ba5c1dc16
tkelley3 <th.kelley@northeastern.edu>
2024-01-14 21:08:01 -0500

    Jan 14 Update

---
+++ estimatepi.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on ...
+
+@author: ...
+Description: ...
+    
+"""
+
+def main():
+    #Program body
+    k=100
+    estpi = (-1)**(k)/(2*k+1)
+    return estpi
+    
+#The code below runs whatever is in main()
+if __name__ == '__main__':
+    main()
