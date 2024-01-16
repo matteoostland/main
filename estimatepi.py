047fe71e13b2f27765f699207967670f4df21f54
tkelley3 <th.kelley@northeastern.edu>
2024-01-16 12:45:59 -0500

    Jan 16 Update

--- estimatepi.py
+++ estimatepi.py
@@ -6,11 +6,16 @@ Created on ...
 Description: ...
     
 """
+import numpy as np
 
 def main():
     #Program body
     k=100
-    estpi = (-1)**(k)/(2*k+1)
+    r = range(k)
+    karray = np.array(r)
+    estpi = 4*(-1)**(karray)/(2*karray+1)
+    estpi = sum(estpi)
+    print("My estimate for pi is",estpi)
     return estpi
     
 #The code below runs whatever is in main()
