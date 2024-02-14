8039d560485db365910800cbfc43721494240a63
tkelley3 <th.kelley@northeastern.edu>
2024-02-14 16:11:33 -0500

    Update Feb 14

--- fun.py
+++ fun.py
@@ -2,7 +2,8 @@
 """
 @author: Tom
 """
-from math import sqrt
+
+import math as mh
 
 def rising1D(v0,g=9.8):
     '''
@@ -29,36 +30,28 @@ def falling1D(h,v0=0):
     # returns the time and velocity of an object falling from h and at an initial velocity v0
     g=9.8
     #solution to quadratic equation
-    time = -v0/g + sqrt(v0**2/g**2+2*h/g)
-    velo = sqrt(v0**2+2*g*h)
+    time = -v0/g + mh.sqrt(v0**2/g**2+2*h/g)
+    velo = mh.sqrt(v0**2+2*g*h)
     return time,velo
 
 def projectile(v0, angle, h):
-    
-    
+    # return time and rang
+    deg2rad = lambda ang: ang*mh.pi/180
+    angle = deg2rad(angle)
+    #component breakdown
+    vx = v0*mh.cos(angle)
+    vy0 = v0*mh.sin(angle)
+    [maxh, t1] = rising1D(vy0)
+    [t2, vf] = falling1D(maxh)
+    # add both times
+    time = t1+t2
+    rang = vx * time
     return time, rang
 
+if __name__ == '__main__':
+    [t,rang] = projectile(54, 40, 40)
+    print(f'This projectile travels {round(rang)} m in {round(t,2)} seconds')
 
 
 
 
-
-
-def something(time, route, yeet):
-    '''
-    
-
-    Parameters
-    ----------
-    time : TYPE
-        DESCRIPTION.
-    route : TYPE
-        DESCRIPTION.
-    yeet : TYPE
-        DESCRIPTION.
-
-    Returns
-    -------
-    None.
-
-    '''
