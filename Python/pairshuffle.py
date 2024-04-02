8fab1e9e658062d0b710ccfa83bcfda3cc7ae5f9
tkelley3 <th.kelley@northeastern.edu>
2024-04-02 11:30:45 -0400

    April 2 Update

--- Python/pairshuffle.py
+++ Python/pairshuffle.py
@@ -6,27 +6,45 @@ Created on Mon Apr  1 16:12:06 2024
 """
 
 import random
+import json as js
+
+def flatten_list(nested_list):
+    return [item for sublist in nested_list for item in sublist]
 
 def group_pairs(names):
-    # Shuffle the list to randomize the order
+    #shuffle the list to randomize the order
     random.shuffle(names)
     
-    # If the number of names is odd, remove one name and print it separately
+    #if the number of names is odd, remove one name and print it separately
     if len(names) % 2 != 0:
         odd_name = names.pop()
         print("Unpaired name:", odd_name)
     
-    # Group the names in pairs and print them
+    #group the names in pairs and print them
     for i in range(0, len(names), 2):
         pair = names[i:i+2]
         print(f"Pair {i/2}:", pair)
     
+def schedule(names):
+    #shuffle list
+    random.shuffle(names)
+    sched = {}
+    for k in range(0,len(names)):
+        print(f'{k}: {names[k]}\n')
+        sched[names[k]] = k
+    return sched
+        
 
 
 
-
-solo = ['Brycen', 'Juan', 'Luisa', 'Gabriela', 'Matthew', 'Jacob', 'Zhihui', 'Colin', 'Tanishka', 'Ethan', 'Scott', 'Payton', 'Kevin', 'Zachary', 'Aidan', 'Andreas', 'Volkan', 'Greyson']
+solo = ['Brycen', 'Juan', 'Luisa', 'Gaby', 'Matthew', 'Jacob', 'Zhihui', 'Colin', 'Tanishka', 'Ethan', 'Scott', 'Payton', 'Kevin', 'Zachary', 'Aidan', 'Andreas', 'Volkan', 'Greyson']
 groups = [['Hayley', 'Ruby'],['Lucas', 'Luis', 'Max'],['Ellis', 'Mia'],['Henry', 'Howard', 'Jeremy'],['Grace', 'Shayna', 'Haven']]
 
 group_pairs(solo)
 group_pairs(groups)
+
+#form schedule
+# groups = flatten_list(groups)
+# slist = schedule(solo+groups)
+# with open('FinalSchedule.dat','w') as writefile:
+#     writefile.write(js.dumps(slist))
