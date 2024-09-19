33e7d616ab6f319d5f3304372f00c9b1305fa0f9
tkelley3 <th.kelley@northeastern.edu>
2024-09-19 16:00:11 -0400

    Update

--- Python/class_estimatepi.py
+++ Python/class_estimatepi.py
@@ -5,7 +5,9 @@ Created on Tue Sep 10 12:14:36 2024
 @author: tomke
 """
 import numpy as np
-k=100000
+k = int(input('Please input an integer number of terms: '))
+print(type(k))
+
 numbers = np.array(range(k))
 series = (-1)**numbers/(2*numbers+1)
 
